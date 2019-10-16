c1ef3cb4850c4c615dbf81fbe5b7ae7bf64e5286
Emil Andreasyan <emil.andreasyan@yahoo.com>
2019-10-16 19:42:34 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,9 +1,43 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 require_relative './course.rb'
 
 class Scraper
+  
+  def get_page
+  doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+  
+  #for each iteration over doc.css(".post") (XML elements), create new instance of Course class, set title, schedule and description to XML elements
+  # doc.css(".post").each do |post|
+  #   course = Course.new
+  #   course.title = post.css("h2")
+  #   course.schedule = post.css(".date").text
+  #   course.description = post.css("p").text
+  # end
+  
+def get_courses
+  self.get_page.css(".post")
+end
+
+def make_courses
+  self.get_courses.each do |post|
+    course = Course.new
+    course.title = post.css("h2").text
+    course.schedule = post.css(".date").text
+    course.description = post.css("p").text
+  end
+end
+
+def make_courses
+  self.get_courses.each do |post|
+    course = Course.new
+    course.title = post.css("h2").text
+    course.schedule = post.css(".date").text
+    course.description = post.css("p").text
+  end
+end
   
   def print_courses
     self.make_courses
@@ -16,7 +50,20 @@ class Scraper
     end
   end
   
+  
+  # def print_courses
+  #   self.make_courses
+  #   Course.all.each do |course|
+  #     if course.title && course.title != ""
+  #       puts "Title: #{course.title}"
+  #       puts " Schedule: #{course.schedule}"
+  #       puts " Description #{course.description}"
+  #     end
+  #   end
+  # end
+  
 end
 
-
+Scraper.new.get_page
+Scraper.new.print_courses
 
