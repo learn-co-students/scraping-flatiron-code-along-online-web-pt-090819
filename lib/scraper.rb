6e8e1f0d8edadb706c5823e2140b87cfd1e810c1
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-10-17 17:36:41 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,17 +6,6 @@ require_relative './course.rb'
 
 class Scraper
 
-  def print_courses
-    self.make_courses
-    Course.all.each do |course|
-      if course.title && course.title != ""
-        puts "Title: #{course.title}"
-        puts "  Schedule: #{course.schedule}"
-        puts "  Description: #{course.description}"
-      end
-    end
-  end
-
   def get_page
     doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
 
@@ -49,7 +38,7 @@ class Scraper
         puts "Title: #{course.title}"
         puts " Schedule: #{course.schedule}"
         puts " Description: #{course.description}"
-        puts "*****" #Personally added for spacing/readability 
+        puts "*****" #Personally added for spacing/readability
       end
     }
   end
